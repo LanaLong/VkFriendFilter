86d2b834e627a971d64c2458718b50ccb6c9de28
lanalong <qmexperience@gmail.com>
2019-03-18 17:41:53 +0300

    testing

--- src/index.js
+++ src/index.js
@@ -5,19 +5,17 @@ VK.init({
     apiId: 6904469
 });
 
-const isMatching = (full, chunk) => new RegExp(chunk, 'i').test(full);
-
 const auth = () => new Promise((resolve, reject) => {
     VK.Auth.login(data => {
         if (data.session) {
             resolve();
         } else {
-            reject(new Error('Не удалось авторизоваться'));
+            reject(new Error('Авторизация не удалась'));
         }
     }, 2);
 });
 
-const vkAPI = (method, params) => {
+const callAPI = (method, params) => {
     params.v = '5.92';
 
     return new Promise((resolve, reject) => {
@@ -31,24 +29,27 @@ const vkAPI = (method, params) => {
     });
 };
 
+const isMatching = (full, chunk) => new RegExp(chunk, 'i').test(full);
+
 const renderList = (items, container) => {
     let template = `
     {{#each items}}
     <div class="friend" data-id="{{id}}" draggable="true">
-        <img class="photo" src="{{photo_50}}" alt="photo">
-        <div class="name">{{first_name}} {{last_name}}</div>
-        <img class="add" src="./img/plus.png" alt="plus">
+    <img class="photo" src="{{photo_50}}" alt="photo">
+    <div class="name">{{first_name}} {{last_name}}</div>
+    <img class="add" src="../src/img/plus.png" alt="plus">
     </div>
     {{/each}}`;
     let render = Handlebars.compile(template);
-
+    
     container.innerHTML = render(items);
+    console.log(items);
 };
 
 let currentDrag;
 
 const handleDragStart = (e, zone) => {
-    e.dataTransfer.setData('text/html', 'dragstart'); // for firefox
+    e.dataTransfer.setData('text/html', 'dragstart');
     currentDrag = { source: zone, node: e.target.closest('.friend') };
 };
 
@@ -76,51 +77,56 @@ const makeDnD = (zones) => {
     });
 };
 
-auth().then(async () => {
+auth()
+.then(async () => {
     try {
         const listFriendsAll = document.querySelector('.all .list');
         const listFriendsFav = document.querySelector('.fav .list');
         const search = document.querySelector('.search');
         const save = document.querySelector('.save');
-        const friends = await vkAPI('friends.get', { fields: 'photo_50' });
+        const friends = await callAPI('friends.get', { fields: 'photo_50' });
 
+
+        console.log('its work outside');
+        
         if (localStorage.all) {
             let allSaved = friends.items.filter(item => localStorage.all.includes(item.id));
             let favSaved = friends.items.filter(item => localStorage.fav.includes(item.id));
-
+            
             renderList({ count: allSaved.length, items: allSaved }, listFriendsAll);
             renderList({ count: favSaved.length, items: favSaved }, listFriendsFav);
         } else {
-            renderList(friends, listFriendsAll);
-        }
+                renderList(friends, listFriendsAll);
+            }
 
-        makeDnD([listFriendsAll, listFriendsFav]);
+            makeDnD([listFriendsAll, listFriendsFav]);
 
-        document.addEventListener('click', (e) => {
-            if (e.target.classList.contains('add')) {
-                if (e.target.closest('.all')) {
-                    listFriendsFav.appendChild(e.target.parentNode);
-                } else {
-                    listFriendsAll.appendChild(e.target.parentNode);
+            document.addEventListener('click', (e) => {
+                console.log('its dont work inside');
+                if (e.target.classList.contains('add')) {
+                    if (e.target.closest('.all')) {
+                        listFriendsFav.appendChild(e.target.parentNode);
+                    } else {
+                        listFriendsAll.appendChild(e.target.parentNode);
+                    }
                 }
-            }
-        });
+            });
 
-        search.addEventListener('keyup', (e) => {
-            if (e.target.tagName === 'INPUT') {
-                let source = document.querySelector(e.target.dataset.source);
-                [...source.children].forEach(item => {
-                    item.style.display = isMatching(item.querySelector('.name').innerText, e.target.value) ? 'block' : 'none';
-                });
-            }
-        });
+            search.addEventListener('keyup', (e) => {
+                if (e.target.tagName === 'INPUT') {
+                    let source = document.querySelector(e.target.dataset.source);
+                    [...source.children].forEach(item => {
+                        item.style.display = isMatching(item.querySelector('.name').innerText, e.target.value) ? 'block' : 'none';
+                    });
+                }
+            });
 
-        save.addEventListener('click', () => {
-            localStorage.all = JSON.stringify([...listFriendsAll.children].map(item => +item.dataset.id));
-            localStorage.fav = JSON.stringify([...listFriendsFav.children].map(item => +item.dataset.id));
-            alert('Списки друзей успешно сохранены!');
-        });
-    } catch (e) {
-        console.error(e.message);
-    }
+            save.addEventListener('click', () => {
+                localStorage.all = JSON.stringify([...listFriendsAll.children].map(item => +item.dataset.id));
+                localStorage.fav = JSON.stringify([...listFriendsFav.children].map(item => +item.dataset.id));
+                alert('Списки друзей сохранены');
+            });
+        } catch (e) {
+            console.error(e.message);
+        }
 });
--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
     plugins: [
         new ExtractTextPlugin('styles.css'),
         new HtmlPlugin({
-            title: 'Geo-response',
+            title: 'VkFriendsFilter',
             template: 'index.hbs',
         }),
         new CleanWebpackPlugin(['dist'])
